3774b95f82d7d39fc6e81ecfe8267b2485b9da62
lnpollola <leandro.pollola@hotmail.com>
2019-04-03 23:25:41 -0300

    LOGIN OK EN TAB 2

--- Aplicaciones/01relVisual_V2/src/app/tabs/tabs.router.module.ts
+++ Aplicaciones/01relVisual_V2/src/app/tabs/tabs.router.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
+            loadChildren: '../login-page/login-page.module#LoginPagePageModule'
           }
         ]
       },
