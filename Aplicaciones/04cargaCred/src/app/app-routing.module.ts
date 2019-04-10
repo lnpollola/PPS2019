a5f0252edeffbdf0e26ddc98913bc8c9e3298f4c
lnpollola <leandro.pollola@hotmail.com>
2019-04-10 00:10:57 -0300

    APLICACIONES 4 Y 5, COMIENZO A DESARROLLARLAS

---
+++ Aplicaciones/04cargaCred/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './login-page/login-page.module#LoginPagePageModule' },
+  { path: 'login-page', loadChildren: './login-page/login-page.module#LoginPagePageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './login-page/login-page.module#LoginPagePageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
