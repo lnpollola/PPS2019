63f8940dd65e09013405c23e37d92f877b242dc7
lnpollola <leandro.pollola@hotmail.com>
2019-04-10 23:37:22 -0300

    CORRIJO LOGIN DE LA APP 4

--- Aplicaciones/04cargaCred_V2/src/app/login-page/login-page.page.ts
+++ Aplicaciones/04cargaCred_V2/src/app/login-page/login-page.page.ts
@@ -1,27 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-login-page',
-//   templateUrl: './login-page.page.html',
-//   styleUrls: ['./login-page.page.scss'],
-// })
-// export class LoginPagePage implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
-
-/**
- * Ionic4 Firebase Starter Kit (https://store.enappd.com/product/firebase-starter-kitionic4-firebase-starter)
- *
- * Copyright © 2019-present Enappd. All rights reserved.
- *
- * This source code is licensed as per the terms found in the
- * LICENSE.md file in the root directory of this source tree.
- */
 import { Component, OnInit } from '@angular/core';
 import { MenuController, Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -59,7 +35,7 @@ export class LoginPagePage implements OnInit {
   }
 
   ngOnInit() {
-   
+
   }
 
    login() {
