b11d2a567f4e9c9ff50ec1335ed844dfe039aec0
lnpollola <leandro.pollola@hotmail.com>
2019-04-11 22:07:05 -0300

    subo cambios en el login, boton para acceso rapido y logout

--- Aplicaciones/01relVisual_V2/src/app/login-page/login-page.page.ts
+++ Aplicaciones/01relVisual_V2/src/app/login-page/login-page.page.ts
@@ -1,36 +1,11 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-login-page',
-//   templateUrl: './login-page.page.html',
-//   styleUrls: ['./login-page.page.scss'],
-// })
-// export class LoginPagePage implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
-
-/**
- * Ionic4 Firebase Starter Kit (https://store.enappd.com/product/firebase-starter-kitionic4-firebase-starter)
- *
- * Copyright © 2019-present Enappd. All rights reserved.
- *
- * This source code is licensed as per the terms found in the
- * LICENSE.md file in the root directory of this source tree.
- */
 import { Component, OnInit } from '@angular/core';
-import { MenuController, Platform, AlertController } from '@ionic/angular';
+import { MenuController, Platform, AlertController,  ActionSheetController, ToastController   } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { Router } from "@angular/router";
 import { LoadingController } from '@ionic/angular';
 
 import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
 import { AuthService } from "../auth.service";
-// import * as firebase from 'firebase/app';
 
 
 @Component({
@@ -45,16 +20,18 @@ export class LoginPagePage implements OnInit {
   routerLink = "";
 
   constructor(
-    private platform: Platform, 
+    // private platform: Platform, 
+    // private splashScreen: SplashScreen,
+    // public util: utilService, 
+    // private menuCtrl: MenuController, 
+    // private authServ: AuthenticationService,
+    // private firebaseAuthentication: FirebaseAuthentication, 
     public loadingController: LoadingController,
     public alertController:AlertController,
-    private splashScreen: SplashScreen,
-    // public util: utilService, 
-    private menuCtrl: MenuController, 
-    // private authServ: AuthenticationService,
-    private firebaseAuthentication: FirebaseAuthentication, 
     private auth: AuthService, 
-    private router: Router
+    private router: Router,
+    public toastController: ToastController,
+    public actionSheetController: ActionSheetController
     ) {
   }
 
@@ -65,16 +42,109 @@ export class LoginPagePage implements OnInit {
    login() {
         
       this.auth.loginUser(this.email,this.password ).then((user) => {
-        this.alertaMensaje(true);  
+
+        this.creoToast(true);  
         this.router.navigateByUrl('/tabs'); 
+
         }
         ) 
         .catch(err=>{
           
-          this.alertaMensaje(false);  
+          this.creoToast(false);  
         });
 
       }
+
+
+      
+  async creoSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Ingresar como ...',
+      cssClass: 'actSheet',
+      buttons: [{
+        text: 'admin',
+        icon: 'finger-print',
+        handler: () => {
+          
+          this.email = "admin@gmail.com";
+          this.password= "admin1111";
+
+        }
+      }, {
+        text: 'invitado',
+        icon: 'information-circle-outline',
+        handler: () => {
+          this.email = "invitado@gmail.com";
+          this.password= "invitado2222";
+        }
+      }, {
+        text: 'usuario',
+        icon: 'person',
+        handler: () => {
+          this.email = "usuario@gmail.com";
+          this.password= "usuario3333";
+        }
+      }, {
+        text: 'anonimo',
+        icon: 'help',
+        handler: () => {
+          this.email = "anonimo@gmail.com";
+          this.password= "anonimo4444";
+        }
+      },{
+        text: 'tester',
+        icon: 'desktop',
+        handler: () => {
+          this.email = "tester@gmail.com";
+          this.password= "tester5555";
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        cssClass: 'btnCancel',
+        role: 'cancel',
+        handler: () => {
+         
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+  
+      async creoToast(rta: boolean) {
+
+        if(rta == true)
+        {
+          const toast = await this.toastController.create({
+            message: 'Autenticación exitosa.',
+            color: 'success',
+            showCloseButton: false,
+            position: 'top',
+            closeButtonText: 'Done',
+            duration: 2000 
+          });
+      
+          toast.present();
+    
+    
+        }
+        else{
+          const toast = await this.toastController.create({
+            message: 'Usuario/contraseña incorrectos.',
+            color: 'danger',
+            showCloseButton: false,
+            position: 'bottom',
+            closeButtonText: 'Done',
+            duration: 2000 
+          });
+      
+          toast.present();
+    
+        }
+       
+          
+      }
+
 
     async alertaMensaje(estado: boolean) {
    
