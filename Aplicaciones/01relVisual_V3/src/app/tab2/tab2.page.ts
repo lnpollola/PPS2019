cd8ad785efbb1dd82737afadaed3fb2991c15e47
lnpollola <leandro.pollola@hotmail.com>
2019-04-20 01:10:09 -0300

    termino pantalla 2 app 1

--- Aplicaciones/01relVisual_V3/src/app/tab2/tab2.page.ts
+++ Aplicaciones/01relVisual_V3/src/app/tab2/tab2.page.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page 
+{
+
+  constructor(public navCtrl: NavController) { }
+  galleryType = 'pinterest';
+
+}
