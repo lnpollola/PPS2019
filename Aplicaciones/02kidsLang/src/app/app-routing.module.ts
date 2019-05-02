c6873cb7eae9b0095538a40f60beb06ebac0335b
Leandro Pollola <leandro.pollola@hotmail.com>
2019-05-02 18:23:58 -0300

    version corregida se ve ok en el cel, antes de intentar sonidos

--- Aplicaciones/02kidsLang/src/app/app-routing.module.ts
+++ Aplicaciones/02kidsLang/src/app/app-routing.module.ts
@@ -9,8 +9,8 @@ const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: '', loadChildren: './login-page/login-page.module#LoginPagePageModule'} ,
   { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
-  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
-  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' }
+  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' }
+  // { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' }
 
 ];
 @NgModule({
