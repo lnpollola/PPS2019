d42108d390d4c498a7c981c8382ce43806004401
Leandro Pollola <leandro.pollola@hotmail.com>
2019-05-04 23:59:24 -0300

    version de relVisual con tabs ok y camara relacionada

--- Aplicaciones/01relVisual_V3/src/app/tab1/tab1.module.ts
+++ Aplicaciones/01relVisual_V3/src/app/tab1/tab1.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-// import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   imports: [
@@ -14,8 +13,5 @@ import { Tab1Page } from './tab1.page';
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page],
-  // providers: [
-  //   Camera
-  // ]
 })
 export class Tab1PageModule {}
--- Aplicaciones/01relVisual_V3/src/app/tab1/tab1.page.ts
+++ Aplicaciones/01relVisual_V3/src/app/tab1/tab1.page.ts
@@ -1,33 +1,139 @@
-import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/Camera/ngx';
+// import { Component } from '@angular/core';
+// import { Camera, CameraOptions } from '@ionic-native/Camera/ngx';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
+import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
+import { File, FileEntry } from '@ionic-native/File/ngx';
+import { HttpClient } from '@angular/common/http';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { Storage } from '@ionic/storage';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
+
+const STORAGE_KEY = 'my_images';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page  implements OnInit {
 
-  image: any; 
+  // image: any; 
+  
+  images = [];
 
-  constructor(private camera: Camera) { }
+  // constructor(private camera: Camera) { }
+
+  
+  constructor(
+    private camera: Camera, 
+    private file: File, 
+    private http: HttpClient, 
+    private webview: WebView,
+    private actionSheetController: ActionSheetController, 
+    private toastController: ToastController,
+    private storage: Storage, 
+    private plt: Platform, 
+    private loadingController: LoadingController,
+    private ref: ChangeDetectorRef, 
+    private filePath: FilePath) { }
  
-  sacarFoto(){
-
-    let options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+  ngOnInit() {
+    // this.plt.ready().then(() => {
+    //   this.loadStoredImages();
+    // });
+  }
+ 
+  // loadStoredImages() {
+  //   this.storage.get(STORAGE_KEY).then(images => {
+  //     if (images) {
+  //       let arr = JSON.parse(images);
+  //       this.images = [];
+  //       for (let img of arr) {
+  //         let filePath = this.file.dataDirectory + img;
+  //         let resPath = this.pathForImage(filePath);
+  //         this.images.push({ name: img, path: resPath, filePath: filePath });
+  //       }
+  //     }
+  //   });
+  // }
+ 
+  pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      let converted = this.webview.convertFileSrc(img);
+      return converted;
     }
-
-    this.camera.getPicture( options )
-    .then( (imageData) => {
-      this.image=(<any>window).Ionic.WebView.convertFileSrc(imageData);
-    })
-    .catch(error =>{
-      console.error( error );
+  }
+ 
+  async presentToast(text) {
+    const toast = await this.toastController.create({
+        message: text,
+        position: 'bottom',
+        duration: 3000
     });
+    toast.present();
+  }
+ 
+  async selectImage() {this.takePicture(this.camera.PictureSourceType.CAMERA);}
+ 
+  takePicture(sourceType: PictureSourceType) {
+    var options: CameraOptions = {
+        quality: 100,
+        sourceType: sourceType,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
+    };
+ 
+    this.camera.getPicture(options).then(imagePath => {
+            var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+            var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+    });
+ 
   }
 
+  //FUNCIONES PARA LOGRAR DIRECTORIOS PATH
+  createFileName() {
+    var d = new Date(),
+        n = d.getTime(),
+        newFileName = n + ".jpg";
+    return newFileName;
+}
+ 
+  copyFileToLocalDir(namePath, currentName, newFileName) {
+      this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(success => {
+          this.updateStoredImages(newFileName);
+      }, error => {
+          this.presentToast('Error while storing file.');
+      });
+  }
+
+  
+  updateStoredImages(name) {
+    this.storage.get(STORAGE_KEY).then(images => {
+        let arr = JSON.parse(images);
+        if (!arr) {
+            let newImages = [name];
+            this.storage.set(STORAGE_KEY, JSON.stringify(newImages));
+        } else {
+            arr.push(name);
+            this.storage.set(STORAGE_KEY, JSON.stringify(arr));
+        }
+ 
+        let filePath = this.file.dataDirectory + name;
+        let resPath = this.pathForImage(filePath);
+ 
+        let newEntry = {
+            name: name,
+            path: resPath,
+            filePath: filePath
+        };
+ 
+        this.images = [newEntry, ...this.images];
+        this.ref.detectChanges(); // trigger change detection cycle
+    });
+  }
 }
