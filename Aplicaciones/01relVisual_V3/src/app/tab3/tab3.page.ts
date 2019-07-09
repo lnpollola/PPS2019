24da01ae999a50e17183ab452e48099ec17bb3c5
Leandro Pollola <leandro.pollola@hotmail.com>
2019-07-09 02:22:14 -0300

    ultima version terminada con graficos

--- Aplicaciones/01relVisual_V3/src/app/tab3/tab3.page.ts
+++ Aplicaciones/01relVisual_V3/src/app/tab3/tab3.page.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
-import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
-import { File, FileEntry } from '@ionic-native/File/ngx';
-import { HttpClient } from '@angular/common/http';
-import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { Storage } from '@ionic/storage';
-import { FilePath } from '@ionic-native/file-path/ngx';
- 
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FirebaseService } from "../services/firebase.service";
+import { Chart } from "chart.js";
+
+
 // import { finalize } from 'rxjs/operators';
 
 const STORAGE_KEY = 'my_images';
@@ -20,107 +16,140 @@ export class Tab3Page  implements OnInit
 {
 
 
-  images = [];
-  // arrayImagenes;
-  // imagenLS;
+  @ViewChild('canvasLindas') canvasLindas;
+  graficoLinda: any;
+  @ViewChild('canvasFeas') canvasFeas;
+  graficoFeas: any;
  
-  constructor(private camera: Camera, private file: File, private http: HttpClient, private webview: WebView,
-    private actionSheetController: ActionSheetController, private toastController: ToastController,
-    private storage: Storage, private plt: Platform, private loadingController: LoadingController,
-    private ref: ChangeDetectorRef, private filePath: FilePath) { }
- 
+  estadisticas: any;
+  // OrdenGenerales: { leyenda: string, votos: number }[] = [];
+  cosaslindas: { leyenda: string, votos: number }[] = [];
+  cosasfeas: { leyenda: string, votos: number }[] = [];
+  // limpiezas: { leyenda: string, votos: number }[] = [];
+
+
+  constructor( private dbService: FirebaseService) {
+
+    this.dbService.getItems("cosasEdificio").then(est => {
+      this.estadisticas = est;
+      // this.agruparOrdenGenerales();
+      // this.crearGraficoOrdenGenerales();
+      this.agruparcosaslindas();
+      this.creargraficoLindas();
+      this.agruparcosasfeas();
+      this.crearGraficocosasfeas();
+     
+    });
+
+   }
+
   ngOnInit() {
-    this.plt.ready().then(() => {
-      this.loadStoredImages();
+  }
+
+
+  agruparcosaslindas() {
+    console.log(this.estadisticas);
+    this.estadisticas.forEach(esta => {
+
+      let result = this.cosaslindas.find(conf => conf.leyenda == esta.likes );
+        if (result) {
+          result.votos += 1;
+        } else {
+          this.cosaslindas.push({ leyenda: esta.likes, votos: 1 });
+        }
+     
     });
   }
 
-  ionViewDidEnter(){
-    this.loadStoredImages();
-  }
- 
-  loadStoredImages() {
-    // localStorage.clear();
-    this.storage.get(STORAGE_KEY).then(images => {
-      if (images) {
-        // console.log("Imagenes:  ", images);
-        let arr = JSON.parse(images);
-        // console.log("Array JSON parse: ", arr);
+  creargraficoLindas() {
+    let leyendas: string[] = [];
+    let valores: number[] = [];
+    this.cosaslindas.forEach(conf => {
+      leyendas.push(conf.leyenda);
+      valores.push(conf.votos);
+    });
+    this.graficoLinda = new Chart(this.canvasLindas.nativeElement, {
 
-        this.images = [];
-
-        for (let img of arr) {
-          // console.log("IMG de array: ", img);
-          let filePath = this.file.dataDirectory + img;
-          let resPath = this.pathForImage(filePath);
-          let varclasificacion =  img.substr(0,img.indexOf('.'));
-          let varfecha =  img.substr(img.indexOf('.')+1,img.lastIndexOf('.')-1);
-          let varUsuario = "admin";
-
-          this.images.push(
-            { name: img,
-              path: resPath,
-              filePath: filePath,
-              clasificacion:varclasificacion,
-              fecha:varfecha,
-              usuario: varUsuario
-            });
-        }
-
-
+      type: 'doughnut',
+      data: {
+        labels: leyendas,
+        datasets: [{
+          label: '',
+          data: valores,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)'
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
       }
     });
+  }
 
-    // this.arrayImagenes = JSON.parse(localStorage.getItem('imagenes'));
-    // for (let img of this.arrayImagenes) 
-    // {
-    //   this.imagenLS = img;
-    //   console.log("Imagen Recorrida: ",img);
-    // }
-    // console.log("ArrayImagenes LS: ",this.arrayImagenes);
-    // console.log("ARRAY DE IMAGENES SALIENDO: ", this.images);
-    
-    // localStorage.setItem('imagen', JSON.stringify(this.images));
-    // console.log("LocalStorage: ", localStorage.getItem('imagen'));
-    // console.log("ARRAY DE IMAGENES JSON: ", JSON.parse(this.images) );
-    
-  }
- 
-  pathForImage(img) {
-    if (img === null) {
-      return '';
-    } else {
-      let converted = this.webview.convertFileSrc(img);
-      return converted;
-    }
-  }
- 
-  async presentToast(text) {
-    const toast = await this.toastController.create({
-        message: text,
-        position: 'bottom',
-        duration: 3000
-    });
-    toast.present();
-  }
- 
 
-  //PARA BORRAR IMAGENES
-  deleteImage(imgEntry, position) {
-    this.images.splice(position, 1);
- 
-    this.storage.get(STORAGE_KEY).then(images => {
-        let arr = JSON.parse(images);
-        let filtered = arr.filter(name => name != imgEntry.name);
-        this.storage.set(STORAGE_KEY, JSON.stringify(filtered));
- 
-        var correctPath = imgEntry.filePath.substr(0, imgEntry.filePath.lastIndexOf('/') + 1);
- 
-        this.file.removeFile(correctPath, imgEntry.name).then(res => {
-            this.presentToast('File removed.');
-        });
+  agruparcosasfeas() {
+    this.estadisticas.forEach(esta => {
+      let result = this.cosasfeas.find(conf => conf.leyenda == esta.likes);
+      if (result) {
+        result.votos += 1;
+      } else {
+        this.cosasfeas.push({ leyenda: esta.likes, votos: 1 });
+      }
     });
-}
+  }
+
+  crearGraficocosasfeas() {
+    let leyendas: string[] = [];
+    let valores: number[] = [];
+    this.cosasfeas.forEach(limpi => {
+      leyendas.push(limpi.leyenda);
+      valores.push(limpi.votos);
+    });
+    this.graficoFeas = new Chart(this.canvasFeas.nativeElement, {
+
+      type: 'bar',
+      data: {
+        labels: leyendas,
+        datasets: [{
+          label: '',
+          data: valores,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)'
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384"
+          ]
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
 
 }
 
