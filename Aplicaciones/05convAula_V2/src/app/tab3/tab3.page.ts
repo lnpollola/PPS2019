274620c25bf94c442eb1d42171e1f56ffcb7decd
Leandro Pollola <leandro.pollola@hotmail.com>
2019-07-11 02:18:13 -0300

    APP ok funcionando 2 canales con distintos estilos

--- Aplicaciones/05convAula_V2/src/app/tab3/tab3.page.ts
+++ Aplicaciones/05convAula_V2/src/app/tab3/tab3.page.ts
@@ -1,8 +1,69 @@
 import { Component } from '@angular/core';
+import { ToastController } from "@ionic/angular";
+import { Router } from "@angular/router";
+import { FirebaseService } from "../services/firebase.service";
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+  public usuario: any;
+  public inputText: string;
+  // public esClienteConPedido: boolean = false;
+  // public esDeliveryBoy: boolean = false;
+  chats: { texto: string, usuario: string, destino: string, hora: string }[] = [];
+  public chatListoEntrega: any[] = [];
+  // public cliente: string = "";
+  usuarioLOG = {
+    "correo": ""
+  }
+
+  constructor(public toastController: ToastController,
+    private baseService: FirebaseService,
+              public router: Router){
+                // this.inicializarChats();
+                let usuarioLogeado = JSON.parse(sessionStorage.getItem('Usuarios'));
+                this.usuarioLOG.correo = usuarioLogeado.correo;
+                this.traerChats();
+
+              }
+
+              
+
+              mainM(){
+                this.router.navigateByUrl('/tabinicial'); 
+              }
+
+              traerChats() {
+                this.baseService.getItems('chat').then(chat => {
+                    this.chats = chat.filter(ch => ch.aula == "PPS-4B" );
+                
+           
+                });
+              }
+
+
+
+              doSend() {
+              let usuarioLogeado = JSON.parse(sessionStorage.getItem('Usuarios'));
+
+                let hora_fecha = (new Date()).toLocaleDateString() + ' ' + (new Date()).toLocaleTimeString();
+                let chat: any;
+                  chat = {
+                    texto: this.inputText,
+                    usuario: usuarioLogeado.correo,
+                    aula: "PPS-4B",
+                    hora: hora_fecha
+                  };
+            
+                this.baseService.addItem('chat', chat);
+                this.inputText = "";
+                this.traerChats();
+              }
+
+
+
+}
